1811ad0d02fd6dc4c131e15a9251568a05a61767
Bruce Gordon <bruceg@yahoo-inc.com>
2015-10-22 13:33:36 -0400

    wip

---
+++ guerilla.rb
@@ -0,0 +1,40 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/homebrew/master/frames
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Guerilla < Formula
+  desc ""
+  homepage ""
+  #url "https://git.corp.yahoo.com/bruceg/yahoo-guerilla/archive/0.1.tar.gz", 
+  url "git@git.corp.yahoo.com:bruceg/yahoo-guerilla.git", :using => :git, :tag =>"0.1"
+  version "0.1"
+  #sha256 "c1ec49f0cc9a13322b487b9170f17aa88f73bd266c3d8ac46cb4aa6b2409a2e0"
+
+
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    # bin.install "guerilla"
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test guerilla`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "true"
+  end
+end
