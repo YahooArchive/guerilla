611430135674dfe766f815b1f9cbf7f2f8994115
Bruce Gordon <bruceg@yahoo-inc.com>
2015-11-04 14:51:30 -0500

    cleanup up startup. now consistently throws exceptions. Cleaned up data_dir creation.

--- worker.js
+++ worker.js
@@ -16,28 +16,26 @@ logger.d('Guerilla worker is starting up...')
 
 
 logger.d('Validating config/worker/config.json...');
-try { config = require('./lib/config'); }
-catch (ex) { return logger.e(ex.message); }
+config = require('./lib/config');
 
-fs.mkdirs(config.data_dir, function (error) {
-	if (error) return logger.e('Failed to create the Guerilla directory. Exiting.');
+logger.d("creating output directory");
+fs.mkdirsSync(config.data_dir);
 
-	logger.d('Connecting to db...');
-	db.connect(config.db, function (error, db) {
-		if (error) return logger.e('Failed to connect to the db. Exiting.');
-		logger.d('Connected to db...');
-		
-		logger.d('Initializing queue...');
-		require('./lib/queue').init(function () {
-			logger.d('Initialized queue...');
-			logger.d('Creating app...');
-			var app = require('./lib/app');
-			logger.d('Created app...');
-			logger.d('Creating server...');
-			http.createServer(app).listen(config.port, function () {
-				logger.d('Created server...');
-				logger.i('Guerilla worker is ready.');
-			});
+logger.d('Connecting to db...');
+db.connect(config.db, function (error, db) {
+	if (error) throw new Error('Failed to connect to the db. Exiting. Error:' + error);
+	logger.d('Connected to db...');
+
+	logger.d('Initializing queue...');
+	require('./lib/queue').init(function () {
+		logger.d('Initialized queue...');
+		logger.d('Creating app...');
+		var app = require('./lib/app');
+		logger.d('Created app...');
+		logger.d('Creating server...');
+		http.createServer(app).listen(config.port, function () {
+			logger.d('Created server...');
+			logger.i('Guerilla worker is ready.');
 		});
 	});
 });
