49c642f729cc4cbc894555418946a4fa381258cf
Bruce Gordon <bruceg@yahoo-inc.com>
2015-12-30 14:09:24 -0500

    wip

--- deploy/scripts/ansible/clone-repo-locally.sh
+++ deploy/scripts/ansible/clone-repo-locally.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-#TODO: make 'bruceg' below a variable
-ansible-playbook -i production-inventory -ubruceg -s clone-repo-locally.yml
+# TODO: parameterize
+# TODO: Get Ansible git to work
+rm -rf /Users/bruceg/repos/ansible-checkout/bruceg-yahoo-guerilla
+git clone --single-branch -b deploy-nyc1 --depth 1 --recurse-submodules git@git.corp.yahoo.com:bruceg/yahoo-guerilla.git /Users/bruceg/repos/ansible-checkout/bruceg-yahoo-guerilla
--- deploy/scripts/setup-vm/generate-vms.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-vmrun stop ~/Documents/Virtual\ Machines.localized/OS\ X\ 10.10.vmwarevm/OS\ X\ 10.10.vmx
-vmrun clone ~/Documents/Virtual\ Machines.localized/OS\ X\ 10.10.vmwarevm/OS\ X\ 10.10.vmx  /Volumes/startech/vm3/GuerillaVMv3.vmx full
-rc=$?; if [[ $rc != 0 ]] then echo 'failure generate-vms.sh 1'; exit $rc; fi
-vmrun clone /Volumes/startech/vm3/GuerillaVMv3.vmx /Volumes/startech/vm3/GuerillaVMv3-linked1.vmx linked
-rc=$?; if [[ $rc != 0 ]] then echo 'failure generate-vms.sh 2'; exit $rc; fi
-vmrun clone /Volumes/startech/vm3/GuerillaVMv3.vmx /Volumes/startech/vm3/GuerillaVMv3-linked2.vmx linked
-rc=$?; if [[ $rc != 0 ]] then echo 'failure generate-vms.sh 3'; exit $rc; fi
